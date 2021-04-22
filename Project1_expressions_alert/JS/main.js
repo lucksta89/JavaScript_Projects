da4e3e63c5eb6fc5db089e0b9bc02e3c640f1b15
lucksta89 <79462805+lucksta89@users.noreply.github.com>
2021-04-22 14:45:36 +0100

    added colour to the strings

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,79 @@
+/* alerts */
+
+        alert ("hello this is an alert")
+
+        b = "this is my second alert"
+        alert(b)
+
+/* alerts */
+
+
+/* strings and variables */
+        document.write("this is my first string")
+
+        var a = "this is my second string"
+        var c = "this is my forth string"
+
+        var d = "\"i have declared a concentrated string to a variable" + " for the first time!\""
+
+        var family = "luck", son1 = "dan", son2 = "matt" , mum = "margaret", dad = "john"
+
+        document.write(a)
+        document.write("\"this is my 3\'rd string\"")
+        document.write("this is a variable ")
+        
+        document.write(a,c)
+
+        document.write(d)
+
+        document.write (dad,mum, family)
+
+/* strings and variables */
+
+
+/* changing the color of variables */
+
+        var e = " this text is blue "
+        
+        var a = a.fontcolor("red")
+        var b = b.fontcolor("green")
+        var c = c.fontcolor("yellow")
+        var d = d.fontcolor("orange")
+        var e = e.fontcolor("lightblue")
+        document.write(a,b,c,d,e)
+
+/* changing the color of variables */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
