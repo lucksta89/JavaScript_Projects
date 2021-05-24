214f7a4efb0c4f08561358b78418c7b7178fe3fe
lucksta89 <79462805+lucksta89@users.noreply.github.com>
2021-05-24 17:14:12 +0100

    removed the slash so the pic will work now

--- Project9_countdown_slideshow/JS/main.js
+++ Project9_countdown_slideshow/JS/main.js
@@ -4,7 +4,7 @@ function countdown(){
     var seconds = document.getElementById("seconds").value;
  
      function tick(){
-         seconds = seconds - 1;
+        var seconds = seconds - 1;
          timer.innerHTML = seconds;
          setTimeout(tick, 1000);
          if(seconds == -1){
