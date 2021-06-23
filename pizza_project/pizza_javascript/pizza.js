2965d3cc95a635090aca6ce6b13a8fc283dcd7f8
lucksta89 <79462805+lucksta89@users.noreply.github.com>
2021-06-23 23:07:57 +0100

    corrected the problem

--- pizza_project/pizza_javascript/pizza.js
+++ pizza_project/pizza_javascript/pizza.js
@@ -13,27 +13,27 @@ function getReceipt() {
     }
 }
     if(selectedSize === "Personal Pizza"){
-        sizeTotal = 6;
+        sizeTotal = 5;
     }
     
     else if(selectedSize === "Small Pizza"){
-        sizeTotal = 8;
+        sizeTotal = 7;
     }
     
     else if(selectedSize === "Medium Pizza"){
-        sizeTotal = 10;
+        sizeTotal = 9;
     }
     
     else if(selectedSize === "Large Pizza"){
-        sizeTotal = 14;
+        sizeTotal = 15;
     }
     
     else if(selectedSize === "Extra Large Pizza"){
-        sizeTotal = 16;
+        sizeTotal = 22;
     }
 
     else if(selectedSize === "Super Large Pizza"){
-        sizeTotal = 18;
+        sizeTotal = 28;
     }
 
     runningTotal = sizeTotal;
@@ -64,19 +64,7 @@ function getTopping(runningTotal,text1) {
     else{
         toppingTotal = 0;
     }
- runningTotal = (runningTotal + toppingTotal);
-    console.log("total selected topping items: "+toppingCount);
-    console.log(toppingCount+" topping -1 free topping = "+"£"+toppingTotal+".00");
-    console.log("topping text1: "+text1);
-    console.log("Purchase Total: "+"£"+runningTotal+".00");   
-    
-    document.getElementById("showText").innerHTML= text1;
-    document.getElementById("totalPrice").innerHTML= "<h3>Total: <strong>£" + runningTotal +".00"+"</strong></h3>";
 
-};
-
-
-function getVeg(goingTotal,text1) {
     var VegTotal = 0;
     var selectedVeg = [];
     var VegArray = document.getElementsByClassName("Veg");
@@ -94,59 +82,17 @@ function getVeg(goingTotal,text1) {
     else{
         VegTotal = 0;
     }
-    goingTotal = (goingTotal + VegTotal);
-    console.log("total selected veg items: "+ VegCount);
-    console.log(VegCount+" veg -1 free topping = "+"£" + VegTotal+".00");
-    console.log("Veg text1: "+ text1);
-    console.log("Purchase Total: "+"£"+ goingTotal+".00");
 
+ runningTotal = (runningTotal + toppingTotal + VegTotal);
+    console.log("total selected topping items: "+toppingCount);
+    console.log(toppingCount+" topping -1 free topping = "+"£"+toppingTotal+".00");
+    console.log("topping text1: "+text1);
+    console.log("Purchase Total: "+"£"+runningTotal+".00");   
+    
     document.getElementById("showText").innerHTML= text1;
-    document.getElementById("totalPrice").innerHTML= "<h3>Total: <strong>£" + goingTotal +".00"+"</strong></h3>";
+    document.getElementById("totalPrice").innerHTML= "<h3>Total: <strong>£" + runningTotal +".00"+"</strong></h3>";
+
 };
-
-
-
-
-
-
-
-
-
-
-
-
-function test() {
-    drink = document.getElementById("games").value;
-     if (drink <= 18 ){
-         a = " you cant purchase alcohol!"
-     }
-     else if (drink > 18){
-         a = "you can buy alcohol with your meal!"
-         a += "<ol><li>beer</li><li>spirits</li></ol>"
-     }
-     document.getElementById("answer").innerHTML = a
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
