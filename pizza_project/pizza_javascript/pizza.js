ff35f18472d30f4f8ab3f531d17fdd0345e7900a
lucksta89 <79462805+lucksta89@users.noreply.github.com>
2021-06-23 22:31:10 +0100

    Update pizza.js

--- pizza_project/pizza_javascript/pizza.js
+++ pizza_project/pizza_javascript/pizza.js
@@ -4,7 +4,6 @@ function getReceipt() {
     //function to function, growing line by line into a full receipt
     var text1 = "<h3>You ordered:</h3>";
     var runningTotal = 0;
-    var goingTotal = 0;
     var sizeTotal = 0;
     var sizeArray = document.getElementsByClassName("size");
     for (var i = 0; i < sizeArray.length;i++){
@@ -25,7 +24,7 @@ function getReceipt() {
         sizeTotal = 10;
     }
     
-    else if(selectedSize === "large Pizza"){
+    else if(selectedSize === "Large Pizza"){
         sizeTotal = 14;
     }
     
@@ -38,12 +37,14 @@ function getReceipt() {
     }
 
     runningTotal = sizeTotal;
-    console.log(selectedSize+"  = £"+sizeTotal+goingTotal+".00");
+    console.log(selectedSize+"  = £"+sizeTotal+".00");
     console.log("size text1: "+text1);
-    console.log("subtotal: £"+ goingTotal + runningTotal+".00");
+    console.log("subtotal: £"+ runningTotal+".00");
     //these variables will get passed on to each function
-    getTopping(runningTotal,text1) + getVeg(goingTotal,text1);
-};
+       getTopping(runningTotal,text1);
+};   
+
+
 
 function getTopping(runningTotal,text1) {
     var toppingTotal = 0;
@@ -67,7 +68,8 @@ function getTopping(runningTotal,text1) {
     console.log("total selected topping items: "+toppingCount);
     console.log(toppingCount+" topping -1 free topping = "+"£"+toppingTotal+".00");
     console.log("topping text1: "+text1);
-    console.log("Purchase Total: "+"£"+runningTotal+".00");
+    console.log("Purchase Total: "+"£"+runningTotal+".00");   
+    
     document.getElementById("showText").innerHTML= text1;
     document.getElementById("totalPrice").innerHTML= "<h3>Total: <strong>£" + runningTotal +".00"+"</strong></h3>";
 
