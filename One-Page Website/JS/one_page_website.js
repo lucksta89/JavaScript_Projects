46954e92c3d9f078a5ae3044a277cbfcacc63f34
lucksta89 <79462805+lucksta89@users.noreply.github.com>
2021-06-27 17:03:00 +0100

    lightbox

--- One-Page Website/JS/one_page_website.js
+++ One-Page Website/JS/one_page_website.js
@@ -17,10 +17,10 @@ function myFunction() {
        var c = document.getElementById("height").value;
       var d = document.getElementById("weight").value;
   
-   if (b < 19){
+   if (b <= 17){
        g = "come back when your old enough!"
    }
-   else if (b > 19){
+   else if (b >= 18){
        g = "your old enough, WELCOME " + a;
    }
     document.getElementById("name1").innerHTML = "your name is: " + a+ "<br>" +"your age is: " + b+"<br>" 
@@ -33,6 +33,47 @@ function myFunction() {
 
 
 
+
+
+  function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+ var slideIndex = 1;
+ showSlides(slideIndex);
+ 
+ function plusSlides(n) {
+   showSlides(slideIndex += n);
+ }
+ 
+ function currentSlide(n) {
+   showSlides(slideIndex = n);
+ }
+ 
+ function showSlides(n) {
+   var i;
+   var slides = document.getElementsByClassName("mySlides");
+   var dots = document.getElementsByClassName("demo");
+   var captionText = document.getElementById("caption");
+   if (n > slides.length) {slideIndex = 1}    
+   if (n < 1) {slideIndex = slides.length}
+   for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+   }
+   for (i = 0; i < dots.length; i++) {
+       dots[i].className = dots[i].className.replace(" active", "");
+   }
+   slides[slideIndex-1].style.display = "block";  
+   dots[slideIndex-1].className += " active";
+   captionText.innerHTML = dots[slideIndex-1].alt;
+
+ }
+ 
+ 
 
 
 
